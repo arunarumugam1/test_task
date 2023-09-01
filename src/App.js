955b2812bc12d867595f91953d0b64c867a97cc3
arunarumugam1 <arunarumugam1121@gmail.com>
2023-09-01 11:30:30 +0530

    not done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signup from "./insta/signup";
-import Signin from "./insta/signin";
-import Dashboard from "./insta/dashboard";
-import Update from "./insta/update";
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Signup/>} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/update/:id" element={<Update />} />
+         
         </Routes>
       </BrowserRouter>
     </>
